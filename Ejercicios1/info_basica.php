b9fd300a3929ffc686f8028aa1933198bd4d19ea
AnthonyAbril <antabrrue@alu.edu.gva.es>
2024-09-19 18:38:14 +0200

    ejercicio1

---
+++ Ejercicios1/info_basica.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="es">
+    <head>
+        <title>Ejercicio1</title>
+    </head>
+    <body>
+        <h1>Página de prueba en PHP</h1>
+        <?php
+            // Variables para almacenar el nombre y el año actual
+            $nombre = "Anthony Abril";
+            $anyo = 2004;
+        ?>
+        <p>Me llamo <?php echo $nombre; ?> y nací en el año <?php echo $anyo; ?>.</p>
+    </body>
+</html>

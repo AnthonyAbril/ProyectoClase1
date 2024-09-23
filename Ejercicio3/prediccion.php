a2bce45c7a6d9bc7d62bda494c874993a4a1d2f4
AnthonyAbril <antabrrue@alu.edu.gva.es>
2024-09-23 16:35:01 +0200

    Ejercicio3

---
+++ Ejercicio3/prediccion.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>        
+    <h1>Página de prueba en PHP</h1>
+    <?php
+        $num1 = 3;
+        $num2 = 5;
+        $num3 = 8;
+        $num1 *= 4;
+        echo $num1;                             //3
+        echo $num1 <= $num2;                    //false
+        echo $num3 > $num1 and $num3 > $num2;   //false
+        echo $num3 > $num1 or $num3 > $num2;    //true
+        echo $num1 > $num2 xor $num1 > $num3;   //false
+        $num3--;
+        echo $num3;                             //7
+        $num3 += $num1;                 
+        echo $num3;                             //19
+    ?>
+</body>
+</html>

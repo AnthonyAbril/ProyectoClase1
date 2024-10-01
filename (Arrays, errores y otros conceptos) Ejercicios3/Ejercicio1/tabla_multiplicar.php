aefd4318ecf2744bb7ad8637e55b865cfee86622
AnthonyAbril <antabrrue@alu.edu.gva.es>
2024-10-01 17:08:08 +0200

    Ejercicios3 empezados

---
+++ (Arrays, errores y otros conceptos) Ejercicios3/Ejercicio1/tabla_multiplicar.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+        $tabla = [][];
+
+        
+    ?>    
+
+    <br/>
+    <p>
+        Crea en ella un array numérico bidimensional donde en cada fila almacenes la tabla de multiplicar 
+        de un número del 0 al 9; debería quedarte un array como éste (sin sacar nada por pantalla):
+
+        0 0 0 0 0 0 0 0 0 0 0
+        0 1 2 3 4 5 6 7 8 9 10
+        0 2 4 6 8 10 12 14 16 18 20
+
+        [0][0] [0][1] [0][2]
+        [1][0] [1][2] [1][3]
+        [2][0] [2][2] [2][3]
+    </p>
+</body>
+</html>
+
